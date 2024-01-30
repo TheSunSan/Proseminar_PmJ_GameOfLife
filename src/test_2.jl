86a6073ece28f25dca7e65f3bf39277b3d49d753
TheSunSan <valentino.bergamotto@gmx.net>
2024-01-30 11:18:22 +0100

    function for options

--- src/test_2.jl
+++ src/test_2.jl
@@ -1,34 +1,3 @@
-include("cell.jl")
+a = readline()
 
-cell1 = Cell() # leere neighbour, alive
-cell2 = Cell() # leere neighbour, alive
-
-cell3 = Cell() # 3 neighbours davon 1 alive, alive
-set_alive(cell3)
-cell4 = Cell() # 3 neighbours davon 1 alive, alive
-set_alive(cell4)
-set_neighbours(cell4, cell1)
-set_neighbours(cell4, cell2)
-set_neighbours(cell4, cell3)
-set_neighbours(cell3, cell1)
-set_neighbours(cell3, cell2)
-set_neighbours(cell3, cell4)
-set_neighbours(cell2, cell1)
-set_neighbours(cell2, cell3)
-set_neighbours(cell2, cell4)
-set_neighbours(cell1, cell2)
-set_neighbours(cell1, cell3)
-set_neighbours(cell1, cell4)
-
-# for c in cell4.neighbours
-#     @show c
-# end
-set_alive(cell2)
-set_alive(cell1)
-
-@show get_living_neighbours(cell3)
-
-@show get_living_neighbours(cell4)
-
-# set_alive(cell2)
-# @show get_living_neighbours(cell4)
+print("a: ",a)
